c8a51bc81062da2da08cae4e987d091840bb7511
Oreo <lichen991118@gmail.com>
2018-10-11 14:58:52 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Button } from 'antd';
+
+ReactDOM.render(
+  <div>aa
+    <Button> sass</Button>a</div>,
+  document.getElementById("root")
+);
